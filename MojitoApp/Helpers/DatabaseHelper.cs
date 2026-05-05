27a8eeb88bf2161e5183e2d88f79d1aade6f36f4
BurdeniucAlexN <alex.burdeniuc9@gmail.com>
2026-05-05 15:09:20 +0300

    Initialize WPF project, add DatabaseHelper and folder structure closes #7

---
+++ MojitoApp/Helpers/DatabaseHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MojitoApp.Helpers
+{
+    public class DatabaseHelper
+    {
+        private static readonly string ConnectionString =
+            "Server=ALEX_COMP1\\SQLEXPRESS;Database=MojitoDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public static bool TestConnection()
+        {
+            try
+            {
+                using var conn = GetConnection();
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MojitoApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MojitoApp.Helpers;
+
+namespace MojitoApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (DatabaseHelper.TestConnection())
+                MessageBox.Show("Conexiune la MojitoDB reușită!", "Success",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("Eroare la conectare!", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
